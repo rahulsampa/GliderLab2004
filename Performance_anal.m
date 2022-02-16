7c721e97a1f8cf2c292b23882ba6c7a8ef28ca77
Noah-Abeson <64713696+Noah-Abeson@users.noreply.github.com>
2022-02-16 11:27:40 -0700

    new endurance

--- Performance_anal.m
+++ Performance_anal.m
@@ -1,11 +1,21 @@
 h = 40;
+W = 2.67; %lbs
+Sref = 240; %in ^2
 
-%% calculating endurance
 
 
 %% calculating range
 range  = (C_L./C_D_min)*h./3.281;
 
 range2 = (.26057/0.01598)*h
+
+
+
+%% calculating endurance
+Wload = W/Sref
+
+% theta = arctan(range2/h)
+% Vvert = V*cos(theta)
+% E = h/Vvert;
 
 plot(C_D_min,range)
